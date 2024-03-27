546d37e31faab7b8489a269ef92d947394c7fc2d
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-03-27 03:35:13 -0700

    Add files via upload

---
+++ CS544/lab1-final/kern/showArt.h
@@ -0,0 +1,12 @@
+#ifndef JOS_INC_SHOW_COLOR_H
+#define JOS_INC_SHOW_COLOR_H
+
+#define C_RED "\033[31m"
+#define C_YLLW "\033[0m"
+#define C_GREN "\033[32m"
+#define C_BLUE "\033[34m"
+#define C_PRPL "\033[35m"
+
+#endif
+
+
